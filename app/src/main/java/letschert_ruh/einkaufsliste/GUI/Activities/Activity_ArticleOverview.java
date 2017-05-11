a33e95105c42529ada0587286d84d2c9ef140992
Marcel Ruh <kontakt@mattelp.de>
2017-05-11 18:10:11 +0200

    Appdesignwechsel
    Appdesign zu Material Design gewechselt

---
+++ app/src/main/java/letschert_ruh/einkaufsliste/GUI/Activities/Activity_ArticleOverview.java
@@ -0,0 +1,15 @@
+package letschert_ruh.einkaufsliste.GUI.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import letschert_ruh.einkaufsliste.R;
+
+public class Activity_ArticleOverview extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__article_overview);
+    }
+}
